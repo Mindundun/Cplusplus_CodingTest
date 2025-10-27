4e63553410e56bf0182598b40b554974b182ce85
Mindun <pmk10077@gmail.com>
2025-10-27 13:29:47 +0900

    추가 설명(cout은 기본적으로 실수를 출력하면 일부분만 출력)

--- .vscode/basic/cout_double/cout_double.cpp
+++ .vscode/basic/cout_double/cout_double.cpp
@@ -5,7 +5,8 @@ typedef long long ll;
 double a = 1.23456789;
 
 int main() {
-    cout << a << "\n";                             // 1.23457
+
+    cout << a << "\n";                             // 1.23457 : cout은 기본적으로 실수를 출력하면 일부분만 출력
     cout << fixed << setprecision(3) << a << "\n"; // 1.235
     cout << fixed << setprecision(8) << a << "\n"; // 1.23456789
 }
